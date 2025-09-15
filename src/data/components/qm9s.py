e303d456957c5b6d615245888a2c4b6eaac1a80a
pengjuRen99 <pengjuren99@163.com>
2025-09-15 15:11:59 +0800

    add qm9s.py

---
+++ src/data/components/qm9s.py
@@ -0,0 +1,61 @@
+import torch
+from torch_geometric.transforms import Compose
+from torch_geometric.datasets import QM9 as QM9_geometric
+from torch_geometric.nn.models.schnet import qm9_target_dict
+
+
+class QM9SP(QM9_geometric):     # 三类光谱，需要一个list，[uv, ir, raman]
+    def __init__(self, root, transform=None, dataset_arg=None):
+        assert dataset_arg is not None, (
+            "Please pass the desired property to "
+            'train on via "dataset_arg". Available '
+            f'properties are {", ".join(qm9_target_dict.values())}.'
+        )
+
+        self.label = dataset_arg
+        if dataset_arg == "alpha":   # set this value as placeholder during pre-training
+            self.label = "isotropic_polarizability"
+        elif dataset_arg in ["U", "U0"]:
+            self.label = "energy_" + dataset_arg
+
+        label2idx = dict(zip(qm9_target_dict.values(), qm9_target_dict.keys()))
+        self.label_idx = label2idx[self.label]
+
+        if transform is None:
+            transform = self._filter_label
+        else:
+            transform = Compose([transform, self._filter_label])
+
+        super(QM9SP, self).__init__(root, transform=transform)
+
+    @property
+    def processed_file_names(self) -> str:
+        # return "data_with_uv_ir_raman.pt"
+        return "data_with_uv_ir_raman_offical.pt"
+
+    def get_atomref(self, max_z=100):
+        atomref = self.atomref(self.label_idx)
+        if atomref is None:
+            return None
+        if atomref.size(0) != max_z:
+            tmp = torch.zeros(max_z).unsqueeze(1)
+            idx = min(max_z, atomref.size(0))
+            tmp[:idx] = atomref[:idx]
+            return tmp
+        return atomref
+
+    def _filter_label(self, batch):
+        batch.y = batch.y[:, self.label_idx].unsqueeze(1)
+        return batch
+
+    def download(self):
+        pass
+
+    def process(self):
+        pass
+
+
+if __name__ == "__main__":
+    dataset = QM9SP(root="/home/RenPengju/codes/multiModal_Fusion/Multimodal_Spectroscopic/data/qm9sp", dataset_arg="homo")
+    print(dataset)
+    print(dataset[0])
