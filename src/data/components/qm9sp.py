f2de322285cc26580f5356b52e0b92d5e49fc48b
pengjuRen99 <pengjuren99@163.com>
2025-09-23 15:57:37 +0800

    update eval configs

--- src/data/components/qm9sp.py
+++ src/data/components/qm9sp.py
@@ -4,6 +4,8 @@ from torch_geometric.datasets import QM9 as QM9_geometric
 from torch_geometric.nn.models.schnet import qm9_target_dict
 from torch_geometric.data import download_url, extract_zip
 import os
+import pandas as pd
+from torch_geometric.data import Data
 
 
 class QM9SP(QM9_geometric):     # 三类光谱，需要一个list，[uv, ir, raman]
@@ -36,8 +38,8 @@ class QM9SP(QM9_geometric):     # 三类光谱，需要一个list，[uv, ir, ram
 
     @property
     def processed_file_names(self) -> str:
-        # return "data_with_uv_ir_raman.pt"
-        return "data_with_uv_ir_raman_offical.pt"
+        return "data_with_uv_ir_raman.pt"
+        # return "data_with_uv_ir_raman_offical.pt"
 
     def get_atomref(self, max_z=100):
         atomref = self.atomref(self.label_idx)
@@ -63,11 +65,42 @@ class QM9SP(QM9_geometric):     # 三类光谱，需要一个list，[uv, ir, ram
     def process(self):
         # 要提前下载QM9数据集，默认地址在../data/QM9/raw/qm9_v3.pt       使用的qm9V3
         # qm9sp
-        data_qm9sp = torch.load(os.path.join(self.raw_dir, "qm9s.pt"))           # root: ../data/QM9SP
+        # edge_index, pos, number, smile, z, quadrupole, octapole, npacharge, dipole, polar, hyperpolar, energy, Hij, Hi, dedipole, depolar, train_dipole, tran_energy,  
+        data_qm9sp = torch.load(os.path.join(self.raw_dir, "qm9s.pt"), weights_only=False)           # root: ../data/QM9SP
+        # print(len(data_qm9sp))      # 129817
         # qm9
-        print(os.path.dirname(self.root))
-        data_list_qm9 = torch.load(os.path.join(os.path.dirname(self.root), "QM9/raw/qm9_v3.pt"))                # root: ../data/QM9/raw/qm9_v3.pt
-        # print
+        # x, edge_index, edge_attr, y, pos, z, smiles, name, idx 
+        # qm9_v3中没有smiles，data_v3中是包含的
+        data_list_qm9 = torch.load(os.path.join(os.path.dirname(self.root), "QM9/raw/qm9_v3.pt"), weights_only=False)                # root: ../data/QM9/raw/qm9_v3.pt
+        # print(len(data_list_qm9))    # 130831  # 
+        # spectra
+        # index, 129817*702
+        UV_spectra = pd.read_csv(os.path.join(self.raw_dir, "uv_boraden.csv"))
+        IR_spectra = pd.read_csv(os.path.join(self.raw_dir, "ir_boraden.csv"))
+        Raman_spectra = pd.read_csv(os.path.join(self.raw_dir, "raman_boraden.csv"))
+        print("Processing QM9SP Files:")
+        # need augment: x, edge_index, edge_attr, y, pos, z, name, idx, uv, ir, raman
+        data_list = []
+        # 由于在计算qm9sp时会丢掉部分qm9中的数据，因此需要逐个判断
+        for i in range(len(data_qm9sp)):
+            for j in range(len(data_list_qm9)):
+                if "gdb_" + str(data_qm9sp[i].number) == data_list_qm9[j]['name']:
+                    data = Data(
+                        x = data_list_qm9[j]['x'],
+                        edge_index=data_qm9sp[i].edge_index,
+                        edge_attr=data_list_qm9[j]['edge_attr'],
+                        y=data_list_qm9[j]['y'],
+                        pos=data_qm9sp[i].pos,
+                        z=data_qm9sp[i].z,
+                        name=data_list_qm9[j]['name'],
+                        idx=data_list_qm9[j]['idx'],
+                        uv=UV_spectra.iloc[i+1].values,
+                        ir=IR_spectra.iloc[i+1].values,
+                        raman=Raman_spectra.iloc[i+1].values,
+                    )
+                    data_list.append(data)
+                    break
+        self.save(data_list, self.processed_paths)
 
 
 if __name__ == "__main__":
